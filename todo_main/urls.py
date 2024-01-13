5482094f3af126d1224b6a085e6283de89b7e22f
Saad Ali <saad@Saads-MacBook-Air.local>
2024-01-13 14:31:49 -0500

    add task functionality complete

--- todo_main/urls.py
+++ todo_main/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.Home, name='Home')
+    path('', views.Home, name='Home'),
+
+    # TO DO
+    path('todo/', include('todos.urls'))
 ]
--- todo_main/views.py
+++ todo_main/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render 
+from todos.models import Task
 
 def Home(request):
-    return render(request, 'home.html')
+    tasks = Task.objects.filter(is_completed = False).order_by('-created_at')
+    context = {
+        'tasks' : tasks,
+    }
+    return render(request, 'home.html', context) 
